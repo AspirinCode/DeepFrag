ae1d69730e00f4b90b8fbfcaad30d64cf177d19c
hcji <ji.hongchao@foxmail.com>
2019-06-16 08:38:50 +0800

    annotate ms

--- DeepFrag/annotate.py
+++ DeepFrag/annotate.py
@@ -7,37 +7,53 @@ Created on Wed Jun  5 10:03:58 2019
 
 import numpy as np
 import pandas as pd
-import rpy2.robjects as robjects
-import rpy2.robjects.numpy2ri as numpy2ri
 from rdkit import Chem
-from rdkit.Chem import Draw
 from rdkit.Chem.Draw import IPythonConsole
 from rdkit.Chem.rdMolDescriptors import CalcExactMolWt, CalcMolFormula
+from pycdk.pycdk import add_formula, subtract_formula, check_formula, getFormulaExactMass
+
+import rpy2.robjects as robjects
+import rpy2.robjects.numpy2ri as numpy2ri
 numpy2ri.activate()
 robjects.r('''source('DeepFrag/metfrag.R')''')
 generateFragments = robjects.globalenv['generateFragments']
 
-
 def annotate_ms(ms_pred, smi, treeDepth=2):
     mzs = np.array(ms_pred['mz'])
     intensities = np.array(ms_pred['intensity'])
-    frags = list(generateFragments(smi, treeDepth=treeDepth)) + [smi]
-    frags_new = [Chem.MolFromSmiles(f) for f in frags]
-    frags_mass = np.array([CalcExactMolWt(f) for f in frags_new])
-    frags_formula = np.array([CalcMolFormula(f) for f in frags_new])
-    summary = pd.DataFrame({'formula': frags_formula, 'mass': frags_mass})
-    summary = summary.drop_duplicates()
-    summary = summary.reset_index(drop=True)
-    ms_new = pd.DataFrame(columns=['mz', 'intensity', 'annotation', 'exact_mass'])
+    mol = Chem.MolFromSmiles(smi)
+    precursor = CalcExactMolWt(mol) + 1.0032
+    formula = CalcMolFormula(mol)
+    frags = np.unique(generateFragments(smi, treeDepth=2))
+    frags_new = np.array([Chem.MolFromSmiles(s) for s in frags])
+    frags_formula = np.unique([CalcMolFormula(f) for f in frags_new])
+    loss_formula = []
+    for f in frags_formula:
+        l = subtract_formula(formula, f)
+        if l == '':
+            continue
+        if check_formula(l):
+            loss_formula.append(l)
+        add_H = add_formula(l, 'H')
+        de_H = subtract_formula(l, 'H')
+        if check_formula(add_H):
+            loss_formula.append(add_H)
+        if check_formula(de_H):
+            loss_formula.append(de_H)
+    loss_formula = np.unique(loss_formula)
+    loss_mass = [getFormulaExactMass(f) for f in loss_formula]
+    ms_new = pd.DataFrame(columns=['mz', 'intensity', 'annotate_loss', 'exact_mass'])
     for i, mz in enumerate(mzs):
-        diff = np.abs(mz - summary['mass'])
-        if min(diff) <= 3:
-            intensity = intensities[i]
-            this = np.where(diff<=3)[0]
-            addH = np.array(np.round(mz - summary['mass'][this]))
-            annotation = [str(this[s]) + '.' + str(int(addH[s])) + 'H' for s in range(len(this))]
-            exact_mass = list(summary['mass'][this] + addH)
-            ms_new.loc[len(ms_new)] = [mz, intensity, annotation, exact_mass]
-    return ms_new, summary
+        intensity = intensities[i]
+        diff = precursor - mz
+        if min(np.abs(loss_mass - diff)) < 0.5:
+            match = np.where(np.abs(loss_mass - diff) < 0.5)[0]
+            annotate_loss = loss_formula[match]
+            accurate_mass = precursor - loss_mass[match]
+        else:
+            annotate_loss = ''
+            accurate_mass = ''      
+        ms_new.loc[len(ms_new)] = [mz, intensity, annotate_loss, accurate_mass]
+    return ms_new
 
 
---
+++ Scripts/annotation_example.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 08:34:12 2019
+
+@author: hcji
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from libmetgem import msp
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem.rdMolDescriptors import CalcExactMolWt, CalcMolFormula
+from rdkit.Chem import AllChem
+from DeepFrag.utils import load_model, ms_correlation
+from DeepFrag.utils import read_ms, morgan_fp, ms2vec, model_predict, plot_compare_ms
+from DeepFrag.loss import pearson, loss
+from DeepFrag.annotate import annotate_ms
+
+from pycdk.pycdk import add_formula, subtract_formula, check_formula, getFormulaExactMass
+import rpy2.robjects as robjects
+import rpy2.robjects.numpy2ri as numpy2ri
+numpy2ri.activate()
+robjects.r('''source('DeepFrag/metfrag.R')''')
+generateFragments = robjects.globalenv['generateFragments']
+
+
+msp_file = 'RIKEN_PlaSMA/RIKEN_PlaSMA_Pos.msp'
+model = load_model('RIKEN_PlaSMA_Pos_10')
+pretrain = load_model('simulated_Pos_10V')
+result = pd.read_csv('Result/RIKEN_PlaSMA_Pos_10.csv')
+
+# parser dataset
+ms = []
+smiles = []
+energies = []
+modes = []
+parser = msp.read(msp_file)
+for i, (params, data) in enumerate(parser):
+    if 'collisionenergy' in params:
+        energy = params['collisionenergy']
+    else:
+        energy = ''
+    if 'precursortype' in params:
+        ion_mode = params['precursortype']
+    else:
+        ion_mode = ''
+    if 'smiles' in params:
+        smi = params['smiles']
+    else:
+        smi = ''
+
+    if energy != '10':
+        continue
+    data = pd.DataFrame(np.array(data))
+    data.columns = ['mz', 'intensity']
+    modes.append(ion_mode)
+    ms.append(data)
+    smiles.append(smi)
+    energies.append(energy)
+summary = pd.DataFrame({'smiles': smiles, 'ion_mode': modes, 'energy': energies})
+
+
+# example 1
+idx = 1297
+smi = smiles[idx]
+mol = Chem.MolFromSmiles(smi)
+ms_pred = model_predict(smi, model)
+ms_pretrain = model_predict(smi, pretrain)
+ms_anno, frags = annotate_ms(ms_pred, smi)
+ms_real = ms[idx]
+plot_compare_ms(ms_real, ms_pred)
+
+# annotation
+mzs = np.array(ms_pred['mz'])
+intensities = np.array(ms_pred['intensity'])
+mol = Chem.MolFromSmiles(smi)
+precursor = CalcExactMolWt(mol) + 1.0032
+formula = CalcMolFormula(mol)
+frags = np.unique(generateFragments(smi, treeDepth=2))
+frags_new = [Chem.MolFromSmiles(s) for s in frags]
+frags_formula = np.unique([CalcMolFormula(f) for f in frags_new])
+loss_formula = []
+for f in frags_formula:
+    l = subtract_formula(formula, f)
+    if l == '':
+        continue
+    if check_formula(l):
+        loss_formula.append(l)
+    add_H = add_formula(l, 'H')
+    de_H = subtract_formula(l, 'H')
+    if check_formula(add_H):
+        loss_formula.append(add_H)
+    if check_formula(de_H):
+        loss_formula.append(de_H)
+loss_formula = np.unique(loss_formula)
+loss_mass = np.array([getFormulaExactMass(f) for f in loss_formula])
+ms_new = pd.DataFrame(columns=['mz', 'intensity', 'annotate_loss', 'exact_mass'])
+for i, mz in enumerate(mzs):
+    intensity = intensities[i]
+    diff = precursor - mz
+    if min(np.abs(loss_mass - diff)) < 0.5:
+        match = np.where(np.abs(loss_mass - diff) < 0.5)[0]
+        annotate_loss = loss_formula[match]
+        accurate_mass = precursor - loss_mass[match]
+    else:
+        annotate_loss = ''
+        accurate_mass = ''
+    ms_new.loc[len(ms_new)] = [mz, intensity, annotate_loss, accurate_mass]
+
+
